c87028a4bf9abe22f1cf260de9386f1b1790c7fa
“Landon <landonwjohnson@gmail.com>
2023-05-23 12:43:54 -0600

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Landon Johnson on 5/23/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
